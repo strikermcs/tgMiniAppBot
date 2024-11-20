d9caf935f37b4fb8b473afb948d938a235435cd7
Pavel Semeniuk <tellermcs@gmail.com>
2024-11-20 10:36:41 +0200

    inital commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import { Telegraf, Markup } from "telegraf";
+import 'dotenv/config'
+
+const token = process.env.TELEGRAM_BOT_TOKEN
+const webAppUrl = process.env.TELEGRAM_MINI_APP_URL
+
+console.log(`Init bot`)
+
+const bot = new Telegraf(token)
+
+bot.command('start', (ctx) => {
+    console.log("Bot started")
+    ctx.reply(
+        'Hello! Press to start the game',
+        Markup.inlineKeyboard([
+            Markup.button.webApp('Open Tap Hero Wars', `${webAppUrl}?ref=${ctx.payload}`)
+        ])
+    )
+})
+
+bot.launch()
